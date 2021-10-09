ebef9444d72560ee8067dc341b3166b932b23f9f
quyenld9699 <quyenld9699@gmail.com>
2021-10-09 15:57:03 +0700

    setup layput

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import Layout from "components/layout/Layout"
 import Head from "next/head"
 import Image from "next/image"
 import { ImagesPath } from "../src/constants/ImagesPath"
 
 export default function Home() {
-    return <div>aaaa</div>
+    return (
+        <>
+            <Head>
+                <title>Trang chủ</title>
+            </Head>
+            <Layout>
+                Đây là layout
+                <img src={ImagesPath.LOGO} />
+            </Layout>
+        </>
+    )
 }
---
+++ src/components/layout/footer/Footer.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Footer() {
+    return <div className="footer">đây là footer</div>
+}
