ea34e5f5df436620470a2b2cea1cd54ee3420465
Manh Hung <mhungict99@gmail.com>
2021-10-09 21:45:48 +0700

    Init base

--- src/app-configs/index.js
+++ src/app-configs/index.js
@@ -1,7 +1,7 @@
 // config for dev;
 export const Configs = {
-    BASE_API: "api.rembacninh.vn/api",
-    DOMAIN: "rembacninh.vn",
+    BASE_API: "",
+    DOMAIN: "",
 }
 
 export const REQUEST_STATE = {
