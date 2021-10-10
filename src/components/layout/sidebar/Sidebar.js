1e373afcd9ccb119d33a3b6681b4567e8077112f
quyenld9699 <quyenld9699@gmail.com>
2021-10-10 08:45:03 +0700

    Sidebar ui

--- src/components/layout/sidebar/Sidebar.js
+++ src/components/layout/sidebar/Sidebar.js
@@ -1,5 +1,73 @@
 import React from "react"
+import { ImagesPath } from "../../../constants/ImagesPath"
+import Link from "next/link"
+import { danhmuc } from "../../../constants/danhmuc"
 
 export default function Sidebar() {
-    return <div className="sidebar">đây là sidebar</div>
+    return (
+        <div className="sidebar">
+            <div className="sidebar-logo">
+                <img src={ImagesPath.LOGO}></img>
+            </div>
+            <ul>
+                <Link href="/" passHref>
+                    <li>
+                        <span>Trang chủ</span>
+                    </li>
+                </Link>
+                <Link href="/gioi-thieu" passHref>
+                    <li>
+                        <span>Giới thiệu</span>
+                    </li>
+                </Link>
+
+                <li className="multichoice">
+                    <span>Sản phẩm</span>
+                    <input id="toggle_submenu_sidebar" type="checkbox" style={{ display: "none" }} />
+                    <label htmlFor="toggle_submenu_sidebar"></label>
+                    <ul style={{ "--i": danhmuc.length }}>
+                        {danhmuc.map((item, index) => {
+                            return (
+                                <Link key={"Sidebar" + index} href={item.url} passHref>
+                                    <li>
+                                        <span>{item.title}</span>
+                                    </li>
+                                </Link>
+                            )
+                        })}
+                    </ul>
+                </li>
+
+                <Link href="/tin-tuc" passHref>
+                    <li>
+                        <span>Tin tức</span>
+                    </li>
+                </Link>
+                <Link href="/lien-he" passHref>
+                    <li>
+                        <span>Liên hệ</span>
+                    </li>
+                </Link>
+                <Link href="#" passHref>
+                    <li>
+                        <span>Đăng nhập</span>
+                    </li>
+                </Link>
+                <Link href="#" passHref>
+                    <li>
+                        <span>Đăng kí</span>
+                    </li>
+                </Link>
+                <Link href="#" passHref>
+                    <li>
+                        <span>Tài khoản</span>
+                    </li>
+                </Link>
+
+                <li>
+                    <span>Đăng xuất</span>
+                </li>
+            </ul>
+        </div>
+    )
 }
