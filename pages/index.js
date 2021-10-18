3be8ad92dc2bc346f9a556eddeb2355c6adc6eac
quyenld9699 <quyenld9699@gmail.com>
2021-10-18 08:31:38 +0700

    cart modal review

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import CardReview from "./../src/components-share/Card/CardReview/CardReview"
 import CarouselBannerHomePage from "./../src/components-share/Carousel/CarouselBannerHomePage"
 import CarouselProduct from "./../src/components-share/Carousel/CarouselProduct"
 import { useState } from "react"
+import ModalReviewCart from "components-share/Modal/ModalReviewCart/ModalReviewCart"
 const reviewCard = {
     imageUrl: "https://bizweb.dktcdn.net/100/367/937/themes/740363/assets/col1.jpg?1630998054887",
     urlPage: "#",
@@ -27,19 +28,34 @@ const productData2 = {
     oldPrice: "120300đ",
     status: "sale",
 }
+const datatest = {
+    quantity: 5,
+    urlImg: "https://bizweb.dktcdn.net/thumb/large/100/367/937/products/2-4d4650c2-dfd4-4467-bcb4-205c692552e1.jpg?v=1615519760717",
+    title: "Cửa lưới chống muỗi loại to to to",
+    price: 100000,
+}
 
 export default function Home() {
-    const [openModal, setOpenModal] = useState(false)
-    const [dataModal, setData] = useState(1)
-    function openModalEvent(dataPro) {
-        setOpenModal(true)
-        setData(dataPro)
+    const [openProductModal, setOpenProductModal] = useState(false)
+    const [dataProductModal, setDataProductModal] = useState(1)
+    function openProductModalEvent(dataPro) {
+        setOpenProductModal(true)
+        setDataProductModal(dataPro)
     }
-    function closeModalEvent() {
-        setOpenModal(false)
+    function closeProductModal() {
+        setOpenProductModal(false)
     }
 
     console.log("HOme")
+    const [openCartModal, setOpenCartModal] = useState(false)
+    const [dataCartModal, setDataCartModal] = useState(1)
+    function openCartModalEvent(dataPro) {
+        setOpenCartModal(true)
+        setDataCartModal(dataPro)
+    }
+    function closeCartModal() {
+        setOpenCartModal(false)
+    }
     return (
         <>
             <Head>
@@ -68,16 +84,20 @@ export default function Home() {
                     </div>
                     <div>
                         <CarouselProduct>
-                            <CardProduct data={productData} openReviewProductModal={() => openModalEvent(2)} />
-                            <CardProduct data={productData2} openReviewProductModal={() => openModalEvent(3)} />
-                            <CardProduct data={productData} openReviewProductModal={() => openModalEvent(4)} />
-                            <CardProduct data={productData2} openReviewProductModal={() => openModalEvent(5)} />
-                            <CardProduct data={productData2} openReviewProductModal={() => openModalEvent(6)} />
-                            <CardProduct data={productData} openReviewProductModal={() => openModalEvent(7)} />
-                            <CardProduct data={productData2} openReviewProductModal={() => openModalEvent(8)} />
-                            <CardProduct data={productData} openReviewProductModal={() => openModalEvent(9)} />
-                            <CardProduct data={productData} openReviewProductModal={() => openModalEvent(10)} />
-                            <CardProduct data={productData2} openReviewProductModal={() => openModalEvent(11)} />
+                            <CardProduct
+                                data={productData}
+                                openReviewProductModal={() => openProductModalEvent(2)}
+                                openReviewCartModal={() => openCartModalEvent()}
+                            />
+                            <CardProduct data={productData2} openReviewProductModal={() => openProductModalEvent(3)} />
+                            <CardProduct data={productData} openReviewProductModal={() => openProductModalEvent(4)} />
+                            <CardProduct data={productData2} openReviewProductModal={() => openProductModalEvent(5)} />
+                            <CardProduct data={productData2} openReviewProductModal={() => openProductModalEvent(6)} />
+                            <CardProduct data={productData} openReviewProductModal={() => openProductModalEvent(7)} />
+                            <CardProduct data={productData2} openReviewProductModal={() => openProductModalEvent(8)} />
+                            <CardProduct data={productData} openReviewProductModal={() => openProductModalEvent(9)} />
+                            <CardProduct data={productData} openReviewProductModal={() => openProductModalEvent(10)} />
+                            <CardProduct data={productData2} openReviewProductModal={() => openProductModalEvent(11)} />
                         </CarouselProduct>
                     </div>
                     <div className="box_title">
@@ -92,7 +112,8 @@ export default function Home() {
                     <div className="box_title">
                         <h4>Tin tức {"&"} kiến thức</h4>
                     </div>
-                    <ModalReviewProduct isOpen={openModal} data={dataModal} closeModalEvent={closeModalEvent} />
+                    <ModalReviewProduct isOpen={openProductModal} data={dataProductModal} closeModalEvent={closeProductModal} />
+                    <ModalReviewCart data={datatest} isOpen={openCartModal} closeModalEvent={closeCartModal} />
                 </Container>
             </Layout>
         </>
--- src/components-share/Modal/ModalReviewCart/ModalReviewCart.js
+++ src/components-share/Modal/ModalReviewCart/ModalReviewCart.js
@@ -1,5 +1,25 @@
 import React from "react"
-
-export default function ModalReviewCart() {
-    return <div></div>
+import CartItem from "./../../Cart/cart_item/CartItem"
+import { Container } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTimes } from "@fortawesome/free-solid-svg-icons"
+export default function ModalReviewCart(props) {
+    const { isOpen, data, closeModalEvent } = props
+    function closeModal() {
+        closeModalEvent()
+    }
+    return (
+        <div className="modal_custom" style={{ display: isOpen ? "block" : "none" }}>
+            <div className="modal_custom-background" onClick={closeModal}>
+                <Container className="modal_custom-content">
+                    <span onClick={closeModal} className="modal_custom-close">
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                    <div className="main_content">
+                        <CartItem data={data} />
+                    </div>
+                </Container>
+            </div>
+        </div>
+    )
 }
