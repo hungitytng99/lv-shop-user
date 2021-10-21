de6caba3692fcb4e59ddeff55ff0822891cce3cc
quyenld9699 <quyenld9699@gmail.com>
2021-10-21 10:31:26 +0700

    add modal layout

---
+++ src/components-share/Modal/ModalLayout.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Container } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTimes } from "@fortawesome/free-solid-svg-icons"
+export default function ModalLayout(props) {
+    const { isOpen, closeModalEvent, children } = props
+    function closeModal() {
+        closeModalEvent()
+    }
+    return (
+        <div className="modal_custom" style={{ display: isOpen ? "block" : "none" }}>
+            <div className="modal_custom-background" onClick={closeModal}></div>
+            <Container className="modal_custom-content">
+                <span onClick={closeModal} className="modal_custom-close">
+                    <FontAwesomeIcon icon={faTimes} />
+                </span>
+                <div className="main_content">{children}</div>
+            </Container>
+        </div>
+    )
+}
--- src/components-share/Modal/ModalReviewCart/ModalReviewCart.js
+++ src/components-share/Modal/ModalReviewCart/ModalReviewCart.js
@@ -3,23 +3,20 @@ import CartItem from "./../../Cart/cart_item/CartItem"
 import { Container } from "react-bootstrap"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTimes } from "@fortawesome/free-solid-svg-icons"
+import ModalLayout from "../ModalLayout"
 export default function ModalReviewCart(props) {
     const { isOpen, data, closeModalEvent } = props
     function closeModal() {
         closeModalEvent()
     }
     return (
-        <div className="modal_custom" style={{ display: isOpen ? "block" : "none" }}>
-            <div className="modal_custom-background" onClick={closeModal}>
-                <Container className="modal_custom-content">
-                    <span onClick={closeModal} className="modal_custom-close">
-                        <FontAwesomeIcon icon={faTimes} />
-                    </span>
-                    <div className="main_content">
-                        <CartItem data={data} />
-                    </div>
-                </Container>
-            </div>
-        </div>
+        <ModalLayout isOpen={isOpen} closeModalEvent={() => closeModalEvent()}>
+            <CartItem data={data} />
+            <CartItem data={data} />
+            <CartItem data={data} />
+            <CartItem data={data} />
+            <CartItem data={data} />
+            <CartItem data={data} />
+        </ModalLayout>
     )
 }
