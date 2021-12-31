d77513dd20fe214326629d4fae205ee02ca5dc2c
NguyenPhucCo <nobic112399@gmail.com>
2022-01-01 00:30:29 +0700

    fix reponsive screen and add map to lien-he page

--- pages/lien-he/index.js
+++ pages/lien-he/index.js
@@ -1,10 +1,10 @@
-import React from "react"
-import Layout from "src/components/layout/Layout"
-import { Col, Container, Row } from "react-bootstrap"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faEnvelope, faMapMarkerAlt, faMobileAlt } from "@fortawesome/free-solid-svg-icons"
-import { faStackExchange } from "@fortawesome/free-brands-svg-icons"
-import Link from "next/link"
+import React from "react";
+import Layout from "src/components/layout/Layout";
+import { Col, Container, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faMapMarkerAlt, faMobileAlt } from "@fortawesome/free-solid-svg-icons";
+import { faStackExchange } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 export default function LienHe() {
     const breadcrumb = [
@@ -45,25 +45,39 @@ export default function LienHe() {
                             </div>
                             <div className="get-customer-info">
                                 <Row>
-                                    <Col ld={12} sm={12} md={12}>
-                                        <h3 style={{ marginBottom: "20px" }}>Liên hệ</h3>
-                                    </Col>
-                                    <Col ld={12} sm={12} md={12}>
-                                        <input placeholder="Họ và tên"></input>
-                                    </Col>
-                                    <Col ld={12} sm={12} md={12}>
-                                        <input placeholder="Email"></input>
-                                    </Col>
-                                    <Col ld={12} sm={12} md={12}>
-                                        <input className="content" placeholder="Nội dung"></input>
-                                    </Col>
-                                    <Col ld={12} sm={12} md={12}>
-                                        <button>Gửi liên hệ</button>
-                                    </Col>
+                                    <div className="information">
+                                        <Col ld={12} sm={12} md={12}>
+                                            <h3 style={{ marginBottom: "20px" }}>Liên hệ</h3>
+                                        </Col>
+                                        <Col ld={12} sm={12} md={12}>
+                                            <input placeholder="Họ và tên"></input>
+                                        </Col>
+                                        <Col ld={12} sm={12} md={12}>
+                                            <input placeholder="Email"></input>
+                                        </Col>
+                                        <Col ld={12} sm={12} md={12}>
+                                            <input className="content" placeholder="Nội dung"></input>
+                                        </Col>
+                                        <Col ld={12} sm={12} md={12}>
+                                            <button>Gửi liên hệ</button>
+                                        </Col>
+                                    </div>
                                 </Row>
                             </div>
                         </Col>
-                        <Col>map</Col>
+                        <Col lg={8} sm={12} md={12}>
+                            <div className="map">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d59587.17725798849!2d105.81996841961926!3d21.024738945589732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1640969310959!5m2!1svi!2s"
+                                    width="100%"
+                                    height="400"
+                                    style={{ border: 0 }}
+                                    allowfullscreen=""
+                                    loading="lazy"
+                                    className="gg-map"
+                                ></iframe>
+                            </div>
+                        </Col>
                     </Row>
                 </Container>
             </Layout>
